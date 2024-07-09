a7bc19af3535b576d992e9e592d19f3ad2916294
ramiro.barrantes@uvm.edu <ramiro.barrantes@uvm.edu>
2024-07-09 10:39:13 -0400

    updated genome

--- indexGenome.sh
+++ indexGenome.sh
@@ -3,7 +3,7 @@
 #SBATCH -p bluemoon # partition name
 #SBATCH -t 0-20:00 # hours:minutes runlimit after which job will be killed
 #SBATCH -c 6 # number of cores requested -- this needs to be greater than or equal to the number of cores you plan to use to run your job
-#SBATCH --mem 16G
+#SBATCH --mem 20G
 #SBATCH --job-name STAR_index # Job name
 #SBATCH --output=%x_%j.out.txt                                                          
 #SBATCH --mail-user=rbarrant@uvm.edu
@@ -11,9 +11,9 @@
 
 cd /users/r/b/rbarrant/projects/nfcorePipelines/eclipseq/genome
 
-singularity exec --bind /users/r/b/rbarrant/projects/nfcorePipelines/eclipseq:/eclipseq docker://brianyee/star:2.7.6a STAR --runThreadN 6 \
+singularity exec --bind /gpfs1/home/r/b/rbarrant/projects/nfcorePipelines/eclipseq/genome/mouse/mm10:/genome docker://brianyee/star:2.7.6a STAR --runThreadN 6 \
 --runMode genomeGenerate \
---genomeDir /eclipseq/genome \
---genomeFastaFiles /eclipseq/genome/hg38.fa \
---sjdbGTFfile /eclipseq/genome/hg38.knownGene.gtf \
+--genomeDir /genome \
+--genomeFastaFiles /genome/GRCm38.p4.genome.fa \
+--sjdbGTFfile /genome/gencode.vM10.annotation.gtf \
 --sjdbOverhang 99
